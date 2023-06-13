fa931ba65d4057df62951adc5a1046024bcdaaea
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-06-13 14:40:26 +0530

    api key added

--- src/components/weather/temp.js
+++ src/components/weather/temp.js
@@ -1,85 +1,72 @@
-import React from 'react'
-import './style.css'
+import React, { useState, useEffect } from "react";
+import Weathercard from "./weathercard";
+import "./style.css";
 
 const Temp = () => {
+    const [searchValue, setSearchValue] = useState("pune");
+    const [tempInfo, setTempInfo] = useState({});
+
+    const getWeatherInfo = async () => {
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=8d21c0ff154ec4c2bfd1dc2a432cab72`;
+
+            let res = await fetch(url);
+            let data = await res.json();
+
+            const { temp, humidity, pressure } = data.main;
+            const { main: weathermood } = data.weather[0];
+            const { name } = data;
+            const { speed } = data.wind;
+            const { country, sunset } = data.sys;
+
+            const myNewWeatherInfo = {
+                temp,
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset,
+            };
+
+            setTempInfo(myNewWeatherInfo);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, []);
+
     return (
         <>
             <div className="wrap">
                 <div className="search">
-                    <input type="search"
-                        placeholder='Search...'
+                    <input
+                        type="search"
+                        placeholder="search..."
                         autoFocus
                         id="search"
-                        className="searchTerm" />
-                    <button className="searchButton">Search</button>
+                        className="searchTerm"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    />
+
+                    <button
+                        className="searchButton"
+                        type="button"
+                        onClick={getWeatherInfo}>
+                        Search
+                    </button>
                 </div>
             </div>
 
-            <article className="widget">
-                <div className="weatherIcon">
-                    <i className={"wi wi-day-sunny"}></i>
-                </div>
-
-                <div className="weatherInfo">
-                    <div className="temperature">
-                        <span>25.5deg</span>
-                    </div>
-                    <div className="description">
-                        <div className="weatherCondition">
-                            Sunny
-                        </div>
-                        <div className="place">
-                            Pune, India
-                        </div>
-                    </div>
-                </div>
-
-                <div className="date">
-                    {new Date().toLocaleString()}
-                </div>
-
-                <div className="extra-temp">
-                    <div className="temp-info-minmax">
-                        <div className="two-sided-section">
-                            <p><i className={"wi wi-sunset"}></i></p>
-                            <p className="extra-info-leftside">
-                                19:19 PM <br />
-                                Sunset
-                            </p>
-                        </div>
-
-                        <div className="two-sided-section">
-                            <p><i className={"wi wi-humidity"}></i></p>
-                            <p className="extra-info-leftside">
-                                19:19 PM <br />
-                                HUmidy
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="weather-extra-info">
-                        <div className="two-sided-section">
-                            <p><i className={"wi wi-humidity"}></i></p>
-                            <p className="extra-info-leftside">
-                                19:19 PM <br />
-                                Pressure
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="two-sided-section">
-                        <p><i className={"wi wi-humidity"}></i></p>
-                        <p className="extra-info-leftside">
-                            19:19 PM <br />
-                            Speed
-                        </p>
-                    </div>
-
-
-                </div>
-            </article>
+            {/* our temp card  */}
+            <Weathercard {...tempInfo} />
         </>
-    )
-}
+    );
+};
 
-export default Temp
+export default Temp;
